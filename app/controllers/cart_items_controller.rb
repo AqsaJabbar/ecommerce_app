5efdb6b15dc03e4d89af71fa8dc9aed9ddc630fd
AqsaJabbar <aqsajabbar7689@gmail.com>
2022-11-04 14:19:07 +0500

    cart_item

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,22 +5,18 @@ class CartItemsController < ApplicationController
   def new
        @cart_item = CartItem.new
     end
-    def create 
-      @cart_item = CartItem.new(cart_item_params)
-      @product = Product.find_by(params[:product_id])
-      if @cart_item.save
-        respond_to do |format|
-          format.js
-          # redirect_to admin_product_path(@product)
-       end 
-        
-          # redirect_to admin_product_path(@product)
-      else
-        flash.alert = "Something went wrong."
-        redirect_to admin_product_path(@product)
-      end
+  def create 
+    @cart_item = CartItem.new(cart_item_params)
+    @product = Product.find_by(params[:product_id])
+    if @cart_item.save
+      flash.alert = "Item added to cart."
+      redirect_to admin_product_path(@product)
+    else
+      flash.alert = "Something went wrong."
+      redirect_to admin_product_path(@product)
     end
-    private
+  end
+  private
     def cart_item_params
       params.require(:cart_item).permit(:product_id,:quantity)
     end
